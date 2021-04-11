c985dd4a80fbf4e6de650db7fc7cdd5342d2d579
pranaya <gpranaya20@gmail.com>
2021-04-12 00:32:06 +0530

    added functions in class

---
+++ AudioFiles/AudioFile.py
@@ -0,0 +1,12 @@
+class AudioFiles:
+    def create_audio(self):
+        pass
+
+    def update_audio(self):
+        pass
+
+    def get_audio(self):
+        pass
+
+    def delete_audio(self):
+        pass
